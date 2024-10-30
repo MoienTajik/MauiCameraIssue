d3712ef4e9eb07087a209f4f5a83336fdf78e5d6
Moien Tajik <moien.tajik@aihr.com>
2024-10-30 20:33:40 +0100

    add url issue project

--- MauiCameraIssue/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiCameraIssue;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
